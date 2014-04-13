7d1ceb1dee804b508b09609d4e20ac84e6b37ab3
Sean <theoxuanx@gmail.com>
2014-04-14 01:28:11 +0800

    Minor Changes

--- Trackboard/MainWindow.xaml.cs
+++ Trackboard/MainWindow.xaml.cs
@@ -13,7 +13,6 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Collections.ObjectModel;
 using System.Windows.Media.Animation;
-
 namespace Trackboard
 {
     /// <summary>
